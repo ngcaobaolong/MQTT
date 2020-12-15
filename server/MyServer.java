da6299ec9adfd0baf874d79dc7aa9716129f991d
bql20000 <bql20000@gmail.com>
2020-12-15 21:50:37 +0700

    finish client

--- server/MyServer.java
+++ server/MyServer.java
@@ -3,7 +3,6 @@ import java.io.*;
 import org.json.*;
 
 public class MyServer implements Runnable {
-    // 2601345
     Socket connection;
     InputStream inpStream;
     OutputStream outStream;
@@ -37,7 +36,7 @@ public class MyServer implements Runnable {
         }
     }
 
-    public void notifyEvent(String topic, String sender, String msg) {
+    public void notifyMessage(String topic, String sender, String msg) {
         System.out.println("[" + topic + "] [" + sender + "]:  " + msg);
     }
 
@@ -54,8 +53,15 @@ public class MyServer implements Runnable {
         out.close();
     }
 
-    public void sendFile(String topic, String filename) throws IOException {
+    public void sendFile(String topic, String sender, String filename) throws IOException {
         int filesize = (int) new File(filename).length();
+        sendToClient("NEW FILE " + topic + " " + sender + " " + filename + " " + filesize);
+
+        BufferedReader bufferRead = new BufferedReader(new InputStreamReader(inpStream));
+        String recvMess = bufferRead.readLine().toUpperCase();
+        if (!recvMess.equals("ACCEPT")) {
+            return;
+        }
 
         InputStream inp = new FileInputStream(new File(filename));
         DataOutputStream dout = new DataOutputStream(outStream);
@@ -67,119 +73,84 @@ public class MyServer implements Runnable {
             data = inp.readNBytes(need);
             sent += need;
             dout.write(data);
-            //System.out.println(sent);
         }
-        //System.out.print("SERVER says FINISH!");
         inp.close();
     }
 
     public void run() {
-        DataInputStream din = new DataInputStream(inpStream);
         BufferedReader bufferRead = new BufferedReader(new InputStreamReader(inpStream));
         try {
-
-//        String recvMess="";
-//        int user_id;
-//        sendToClient("NEW FILE cat duy text.txt 5");
-//        try {
-//            //recvMess = bufferRead.readLine();
-//
-//            sendFile("cat", "text.txt");
-//            while (true) {
-//                int a = 5;
-//                if (a < 3) break;
-//
-//            }
-//            connection.close();
-//            System.out.println("A client disconnected.");
-//        }
-//        catch (IOException e) {
-//
-//            System.out.println(e);
-//        }
-
-
-
             String recvMess;
-            int cnt = 0;
             while(true) {
-//                recvMess = din.readUTF();
-//                System.out.println("Received from client: " + recvMess);
-//
-//                JSONObject obj = new JSONObject(recvMess);
-//                JSONObject payload = (JSONObject) obj.getJSONObject("payload");
-//
-//                String action = String.valueOf(obj.get("action")).toUpperCase();
-//                String sender = String.valueOf(payload.get("username"));
-//
-//                System.out.println(obj);
-//                //receiveFile("Ocean.gif", 2601345);
-
-
-                if (cnt == 0) {
-                    ++cnt;
-                    sendToClient("NEW FILE cat duy Ocean.gif 2601345");
-                    //recvMess = bufferRead.readLine();
-                    sendFile("cat", "Ocean.gif");
+                recvMess = bufferRead.readLine();
+                System.out.println(recvMess);
+                if (recvMess.equals("QUIT")) {
+                    System.out.println("A client offline.");
                     break;
                 }
 
-                if (cnt < 0) break;
+                System.out.println("Received from client: " + recvMess);
+
+                JSONObject obj = new JSONObject(recvMess);
+                JSONObject payload = (JSONObject) obj.getJSONObject("payload");
+
+                String action = String.valueOf(obj.get("action")).toUpperCase();
+                String sender = String.valueOf(payload.get("username"));
+
+                //System.out.println(obj);
 
 
-//                if (action.equals("LOGIN")) {
-//                    /*
-//                        check for not duplicate username
-//                        login
-//                    */
-//                } else
-//                if (action.equals("LOGOUT")) {
-//                    /*
-//                        logout
-//                    */
-//                } else
-//                if (action.equals("SUBSCRIBE")) {
-//                    /*
-//                        check for valid username & topic
-//                        subscribe
-//                    */
-//                } else
-//                if (action.equals("UNSUBSCRIBE")) {
-//                    /*
-//                        check for valid username & topic
-//                        unsubscribe
-//                    */
-//                } else
-//                if (action.equals("CHAT")) {
-//                    String topic = String.valueOf(payload.get("topic"));
-//                    String message = String.valueOf(payload.get("message"));
-//                    /*
-//                        check for valid username & topic
-//                        sends message to topic
-//                    */
-//                    notifyEvent(topic, sender, message);
-//                } else
-//                if (action.equals("FILE")) {
-//                    String topic = String.valueOf(payload.get("topic"));
-//                    String message = String.valueOf(payload.get("message"));
-//                    String filename = String.valueOf(payload.get("filename"));
-//                    int filesize = (Integer) payload.get("filesize");
-//                    /*
-//                        check for valid username & topic
-//                        sends file to topic
-//                    */
-//                } else
-//                if (action.equals("QUIT")) {
-//                    break;
-//                }
+                if (action.equals("LOGIN")) {
+                    /*
+                        check for not duplicate username
+                        login
+                    */
+                } else
+                if (action.equals("LOGOUT")) {
+                    /*
+                        logout
+                    */
+                } else
+                if (action.equals("SUBSCRIBE")) {
+                    /*
+                        check for valid username & topic
+                        subscribe
+                    */
+                } else
+                if (action.equals("UNSUBSCRIBE")) {
+                    /*
+                        check for valid username & topic
+                        unsubscribe
+                    */
+                } else
+                if (action.equals("CHAT")) {
+                    String topic = String.valueOf(payload.get("topic"));
+                    String message = String.valueOf(payload.get("message"));
+                    /*
+                        check for valid username & topic
+                        sends message to topic
+                    */
+                    notifyMessage(topic, sender, message);
+                } else if (action.equals("FILE")) {
+                    String topic = String.valueOf(payload.get("topic"));
+                    String filename = String.valueOf(payload.get("filename"));
+                    int filesize = (Integer) payload.get("filesize");
+                    /*
+                        check for valid username & topic
+                        sends file to topic
+                    */
+
+                    //for testing purpose only
+                    receiveFile(filename, filesize);
+                    sendFile(topic, sender, filename);
+                    //
+                }
             }
-            din.close();
         } catch (Exception e) {
             System.out.println(e);
         }
 
         try {
-            System.out.println("HUHHHUHu");
             connection.close();
         }
         catch (Exception e) {
